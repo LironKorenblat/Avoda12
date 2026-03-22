d8f0e81cd8e1c1087441003ba3870864d5e3903f
liron_korenblat <lironkorenblat14@gmai.com>
2026-03-22 20:39:51 +0200

    second commit

--- app/src/main/java/com/example/avoda12/MainActivity.java
+++ app/src/main/java/com/example/avoda12/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.avoda12;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,50 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn;
+Switch sw;
+ToggleButton tb;
+int i = 0;
+int j = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btn = findViewById(R.id.btn);
+        sw = findViewById(R.id.sw);
+        tb = findViewById(R.id.tb);
+    }
+
+    public void switchClick(View view) {
+        if(sw.isChecked()){
+            j = 1;
+        }
+        else{
+            j = 0;
+        }
+    }
+
+    public void tbClick(View view) {
+        if(tb.isChecked()){
+            i = 1;
+        }
+        else{
+            i = 0;
+        }
+    }
+
+    public void click(View view) {
+        if (i == 0 && j == 0) {
+
+        }
+        else if (i == 1 && j == 0) {
+        }
+        else if (i == 0 && j == 1) {
+        }
+        else {
+        }
+        i = 0;
+        j = 0;
     }
 }
+
